c135aa7357d849c7ba02cb10a55f6ba399f0541e
Edijay15 <david.edijala@aun.edu.ng>
2024-12-05 15:34:13 +0100

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     #Users
     path('create-user/', user_create),
     path('check-user/', UserData.as_view()),
-    path('update-user-names/', UserRetrieveUpdateView.as_view()),
+    path('update-user-names/<int:pk>/', UserRetrieveUpdateView.as_view()),
     
     # Categories
     path('create-categories/', CategoryCreateView.as_view()),
