77755f36ba7c78ef2eb003cd340416a7e793e456
David Edijala <david.edijala@aun.edu.ng>
2025-01-05 01:38:39 +0100

    Pagination Update

--- chatbot/pagination.py
+++ chatbot/pagination.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 
 
 DEFAULT_PAGE = 1
-DEFAULT_PAGE_SIZE = 6
+DEFAULT_PAGE_SIZE = 15
 
 class CustomPagination(PageNumberPagination):
     """Custom Pagination class to reduce the number of items gotten based on requests."""
